1bbc620819943da5535589289a617b9ba5c2141f
Pramod Rajapaksha <prajapaksha@datacolor.com>
2020-04-19 14:13:19 -0400

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,7 +34,14 @@ namespace API
             services.AddApplicationServices();
 
             services.AddSwaggerDocumentation();
-            
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            }
+            );
 
         }
 
@@ -48,6 +55,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
